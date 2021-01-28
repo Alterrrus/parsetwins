cdf9f1f65e346032405a508423e0a5465c656bea
Andrey Sablin <void.st@gmail.com>
2021-01-28 16:01:01 +0300

    init_commit

---
+++ src/main/java/com/regexp/parsetwins/ParsetwinsApplication.java
@@ -0,0 +1,13 @@
+package com.regexp.parsetwins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParsetwinsApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ParsetwinsApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/regexp/parsetwins/ParsetwinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.regexp.parsetwins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParsetwinsApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
